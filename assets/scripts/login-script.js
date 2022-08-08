b66352b633e260c4c9f8d551bb9e014a062891fc
sahruliman28 <84514705+sahruliman28@users.noreply.github.com>
2022-08-09 00:22:50 +0700

    Update login-script.js
    Menambahkan beberapa komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,13 +5,16 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+// Membuat variabel dari tiap elemen
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// Menginisialisasi variabel
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+// Membuat button submit
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
